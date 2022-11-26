5d799d01d272bac2f047df3f89ef01e880a64ff9
Edusus <71228669+Edusus@users.noreply.github.com>
2022-11-26 12:52:45 -0400

    Delete offside directory

--- offside/routes/api.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const apiUsersRouter = require('./api/users');
-const apiChromesRouter=require('./api/chromes');
-
-router.use('/users',apiUsersRouter);
-router.use('/chromes',apiChromesRouter);
-
-module.exports = router;
