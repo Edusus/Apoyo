bb0ad4f479dc383482aef24f77b818832c27da2a
Edusus <71228669+Edusus@users.noreply.github.com>
2022-12-27 17:02:27 -0400

    Delete project-ids-backend directory

--- project-ids-backend/controllers/teamsControllers.js
+++
@@ -1,8 +0,0 @@
-const finder = require('./teams/finder');
-const poster = require('./teams/poster');
-const updater = require('./teams/updater');
-const deleter = require('./teams/deleter');
-
-module.exports = { 
-  finder, poster, updater, deleter
-}
